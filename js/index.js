a349d48625ef3c0b95651c1166baa72901ba228a
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-02-10 13:52:56 -1000

    finished project

--- js/index.js
+++ js/index.js
@@ -40,3 +40,54 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+const newNav = document.querySelectorAll('nav a');
+newNav[0].textContent=siteContent['nav']['nav-item-1']
+newNav[1].textContent=siteContent['nav']['nav-item-2']
+newNav[2].textContent=siteContent['nav']['nav-item-3']
+newNav[3].textContent=siteContent['nav']['nav-item-4']
+newNav[4].textContent=siteContent['nav']['nav-item-5']
+newNav[5].textContent=siteContent['nav']['nav-item-6']
+
+
+ 
+let newElement = document.createElement("br");
+let h1Name = document.querySelector("h1");
+
+h1Name.innerHTML = "Dom" + "<br>" + "Is" + "<br>" + "Is Awesome";
+
+let newbutton = document.querySelector("button");
+newbutton.textContent = "Get Started";
+
+let picture = document.getElementById("cta-img");
+picture.setAttribute('src', siteContent["cta"]["img-src"]);
+
+const newh4 = document.querySelectorAll('h4');
+newh4[0].textContent=siteContent['main-content']['features-h4']
+newh4[1].textContent=siteContent['main-content']['about-h4']
+newh4[2].textContent=siteContent['main-content']['services-h4']
+newh4[3].textContent=siteContent['main-content']['product-h4']
+newh4[4].textContent=siteContent['main-content']['vision-h4']
+newh4[5].textContent=siteContent['contact']['contact-h4']
+
+
+const newP = document.querySelectorAll('p');
+newP[0].textContent=siteContent['main-content']['features-content']
+newP[1].textContent=siteContent['main-content']['about-content']
+newP[2].textContent=siteContent['main-content']['services-content']
+newP[3].textContent=siteContent['main-content']['product-content']
+newP[4].textContent=siteContent['main-content']['vision-content']
+newP[5].textContent=siteContent['contact']['address']
+newP[6].textContent=siteContent['contact']['phone']
+newP[7].textContent=siteContent['contact']['email']
+newP[8].textContent=siteContent['footer']['copyright']
+
+let newpicture = document.getElementById("middle-img");
+newpicture.setAttribute("src", siteContent["main-content"]["middle-img-src"])
+
+
+
+
+
+
